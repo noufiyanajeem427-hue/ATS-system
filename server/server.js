335da4103aeca0f9e11e00516d9d65c70e7b825a
AishwaryaJalgi <jalgiaishwarya56@gmail.com>
2026-07-22 18:55:28 +0530

    Connected backend to MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const connectDB = require("./config/db");
 
 const app = express();
-
+connectDB();
 app.use(cors());
 app.use(express.json());
 
