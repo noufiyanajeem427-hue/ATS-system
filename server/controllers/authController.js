48e04fa3182834e8adb670382f0c1c4f6d0a5fd8
AishwaryaJalgi <jalgiaishwarya56@gmail.com>
2026-07-24 11:45:11 +0530

    completed authentication module with JWT

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -75,8 +75,27 @@ const loginUser = async (req, res) => {
     });
   }
 };
+const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json(user);
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
 module.exports = {
   registerUser,
   loginUser,
+  getUserProfile,
 };
