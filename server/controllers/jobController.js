9d821a2a89b57f1b5b3f6966e5160ce01fbf8934
AishwaryaJalgi <jalgiaishwarya56@gmail.com>
2026-07-25 17:41:53 +0530

    Added resume upload and job management APIs

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -27,7 +27,6 @@ const createJob = async (req, res) => {
 const getAllJobs = async (req, res) => {
   try {
     const jobs = await Job.find();
-
     res.status(200).json(jobs);
   } catch (error) {
     res.status(500).json({
@@ -55,8 +54,35 @@ const getJobById = async (req, res) => {
   }
 };
 
+// Update job
+const updateJob = async (req, res) => {
+  try {
+    const job = await Job.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!job) {
+      return res.status(404).json({
+        message: "Job not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Job updated successfully",
+      job,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   createJob,
   getAllJobs,
   getJobById,
+  updateJob,
 };
--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -3,18 +3,23 @@ const {
   createJob,
   getAllJobs,
   getJobById,
+  updateJob,
 } = require("../controllers/jobController");
+
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-// Create a new job
+// Create a job
 router.post("/", protect, createJob);
 
 // Get all jobs
 router.get("/", protect, getAllJobs);
 
-// Get job by ID
+// Get a job by ID
 router.get("/:id", protect, getJobById);
+
+// Update a job
+router.put("/:id", protect, updateJob);
 
 module.exports = router;
