a0de8d958cbf537ba60ced0123f2e142de8dda7d
AishwaryaJalgi <jalgiaishwarya56@gmail.com>
2026-07-27 17:33:12 +0530

    Added Notification and resume uploads APIs

--- server/server.js
+++ server/server.js
@@ -8,16 +8,22 @@ const applicationRoutes = require("./routes/applicationRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
 const savedJobRoutes = require("./routes/savedJobRoutes");
 const interviewRoutes = require("./routes/interviewRoutes");
+const notificationRoutes = require("./routes/notificationRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
+const path = require("path");
 const app = express();
 connectDB();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/interviews", interviewRoutes);
 app.use("/api/saved-jobs", savedJobRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/applications", applicationRoutes);
 app.use("/api/jobs", jobRoutes);
+app.use("/api/notifications", notificationRoutes);
 console.log("Auth routes loaded successfully");
 
 app.get("/", (req, res) => {
