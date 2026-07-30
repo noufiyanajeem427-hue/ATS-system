395b85e30908f676d318193673ee2d6143811b71
AishwaryaJalgi <jalgiaishwarya56@gmail.com>
2026-07-30 22:37:50 +0530

    Added user profile fields

--- server/models/user.js
+++ server/models/user.js
@@ -45,6 +45,123 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
+
+    title: {
+      type: String,
+      default: "",
+    },
+
+    bio: {
+      type: String,
+      default: "",
+    },
+
+    phone: {
+      type: String,
+      default: "",
+    },
+
+    location: {
+      type: String,
+      default: "",
+    },
+
+    website: {
+      type: String,
+      default: "",
+    },
+
+    linkedin: {
+      type: String,
+      default: "",
+    },
+
+    github: {
+      type: String,
+      default: "",
+    },
+
+    skills: {
+      type: [String],
+      default: [],
+    },
+
+    experiences: [
+      {
+        company: {
+          type: String,
+          default: "",
+        },
+        role: {
+          type: String,
+          default: "",
+        },
+        period: {
+          type: String,
+          default: "",
+        },
+        location: {
+          type: String,
+          default: "",
+        },
+        description: {
+          type: String,
+          default: "",
+        },
+        current: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    ],
+
+    education: [
+      {
+        school: {
+          type: String,
+          default: "",
+        },
+        degree: {
+          type: String,
+          default: "",
+        },
+        field: {
+          type: String,
+          default: "",
+        },
+        year: {
+          type: String,
+          default: "",
+        },
+        gpa: {
+          type: String,
+          default: "",
+        },
+      },
+    ],
+
+    preferences: {
+      role: {
+        type: String,
+        default: "",
+      },
+      type: {
+        type: String,
+        default: "",
+      },
+      workMode: {
+        type: String,
+        default: "",
+      },
+      salary: {
+        type: String,
+        default: "",
+      },
+      availability: {
+        type: String,
+        default: "",
+      },
+    },
   },
   {
     timestamps: true,
