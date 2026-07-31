f984f66373ee70f9641277b16de50faf9409578e
AishwaryaJalgi <jalgiaishwarya56@gmail.com>
2026-07-31 22:54:30 +0530

    Implemented authentication, user profile, company, job CRUD , and added simple job data

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,14 +1,23 @@
 const express = require("express");
-const { registerUser,loginUser,getUserProfile,uploadResume,updateProfile,getAllUsers} = require("../controllers/authController");
-const {protect} = require("../middleware/authMiddleware");
+const {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  uploadResume,
+  updateProfile,
+  getAllUsers,
+} = require("../controllers/authController");
+
+const { protect } = require("../middleware/authMiddleware");
 const upload = require("../middleware/uploadMiddleware");
+
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/users", getAllUsers);
 router.get("/profile", protect, getUserProfile);
-router.post("/upload-resume", protect, upload.single("resume"), uploadResume);
 router.put("/profile", protect, updateProfile);
+router.post("/upload-resume", protect, upload.single("resume"), uploadResume);
 
 module.exports = router;
